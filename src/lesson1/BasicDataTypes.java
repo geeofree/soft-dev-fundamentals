aaa5b3100b42f537e10ecccb75dab78daaba6f02
Geoffrey <geo.galagaran@gmail.com>
2017-07-19 16:28:57 +0800

    Create basic data type class lesson example

---
+++ src/lesson1/BasicDataTypes.java
@@ -0,0 +1,24 @@
+package lesson1;
+
+public class BasicDataTypes {
+
+	public static void main(String[] args) {
+		//String and Character types
+		String helloWorld = "hello world!";
+		char letterH = 'h';
+		
+		//Numerical types
+		int five = 5;
+		double PI = 3.14159;
+		
+		//Boolean type
+		Boolean isTheLetterH = letterH == 'h';
+		
+		System.out.println(helloWorld);
+		System.out.println(letterH);
+		System.out.println(five);
+		System.out.println(PI);
+		System.out.println(isTheLetterH);
+	}
+
+}
